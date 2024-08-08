12db974b4b1f7d244ec9ae36715a22f2afa37b96
alanderos <alanderoscontreras@gmail.com>
2024-08-07 23:51:24 -0600

    tic tac toe gameplay and definition of winner

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -8,13 +8,22 @@ const TURNS = {
   O: 'o'
 }
 
-
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
 const Square = ({children, isSelected, updateBoard, index}) => {
   const className = `square ${isSelected ? 'is-selected':''}`
 
   const handleClick = () => {
-    updateBoard()
+    updateBoard(index)
   }
   return(
     <div onClick={handleClick} className={className}>
@@ -23,12 +32,45 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   )
 }
 
+
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null)) //useState(['x','x','x','o','o','o','x','o','x'])
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) //null no hay ganador, false es un empate
 
-  const updateBoard = () => {
+  const checkWinner = (boardToCheck) =>{
+    //revisamos todas las combinaciones ganadoras para ver si x u o ganó
+    for(const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
+  const updateBoard = (index) => {
+    //no actualizar posición si hay algo
+    if(board[index] || winner) return //si ya hay algo, sale sin hacer nada
+    //actualizar tablero
+    const newBoard = [ ... board]
+    newBoard[index] = turn
+    setBoard(newBoard) //actualización asincrona
+    //cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+      console.log("winner: ", newWinner)
+    }
+    
   }
 
   return (
