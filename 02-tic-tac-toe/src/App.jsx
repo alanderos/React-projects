6284b2ec1dda951533935f94c271537ad84a61b4
alanderos <alanderoscontreras@gmail.com>
2024-08-20 21:16:55 -0600

    created end of game conditions win/lose/draw

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -54,6 +54,18 @@ function App() {
     return null
   }
 
+  const resetGame = () =>{
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const checkEndGame = (newBoard) => {
+    //revisamos si hay empate
+    //si no hay mas espacios vacios en el tablero
+    return newBoard.every((square)=> square!==null)
+  }
+
   const updateBoard = (index) => {
     //no actualizar posición si hay algo
     if(board[index] || winner) return //si ya hay algo, sale sin hacer nada
@@ -69,6 +81,8 @@ function App() {
     if(newWinner){
       setWinner(newWinner)
       console.log("winner: ", newWinner)
+    } else if (checkEndGame(newBoard)){
+      setWinner(false)
     }
     
   }
@@ -76,6 +90,7 @@ function App() {
   return (
     <main className='board'>
       <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Reset del Juego</button>
       <section className='game'>
         {
           board.map((_,index)=>{
@@ -95,6 +110,27 @@ function App() {
         <Square isSelected = {turn===TURNS.X}>{TURNS.X}</Square>
         <Square isSelected = {turn===TURNS.O}>{TURNS.O}</Square>
       </section>
+      {
+        winner !== null && (
+        <section className='winner'>
+          <div className='text'>
+            <h2>
+              {
+                winner === false
+                ? 'Empate': 'Gano:'
+              }
+            </h2>
+            <header className='win'>
+              {winner && <Square>{winner}</Square>}
+            </header>
+            <footer>
+              <button onClick={resetGame}>Empezar de nuevo</button>
+            </footer>
+          </div>
+          
+        </section>
+      )
+      }
     </main>
   )
 }
