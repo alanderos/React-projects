c79fff13e405ee9e4244ca14b9b2db2394de4753
alanderos <alanderoscontreras@gmail.com>
2024-08-20 23:19:02 -0600

    separating into components

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -3,59 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import confetti from 'canvas-confetti'
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-const WINNER_COMBOS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
-
-const Square = ({children, isSelected, updateBoard, index}) => {
-  const className = `square ${isSelected ? 'is-selected':''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-  return(
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
-
-
+import { Square } from './components/Square'
+import { TURNS,WINNER_COMBOS } from './constants'
+import { checkWinnerFrom } from './logic/board'
+import {WinnerModal} from './components/WinnerModal'
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null)) //useState(['x','x','x','o','o','o','x','o','x'])
   const [turn, setTurn] = useState(TURNS.X)
   const [winner, setWinner] = useState(null) //null no hay ganador, false es un empate
 
-  const checkWinner = (boardToCheck) =>{
-    //revisamos todas las combinaciones ganadoras para ver si x u o ganó
-    for(const combo of WINNER_COMBOS){
-      const [a, b, c] = combo
-      if(
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a]
-      }
-    }
-    return null
-  }
 
   const resetGame = () =>{
+    //function resetGame(){
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
@@ -78,7 +38,7 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
     //revisar si hay ganador
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinnerFrom(newBoard)
     if(newWinner){
       confetti()
       setWinner(newWinner)
@@ -112,27 +72,7 @@ function App() {
         <Square isSelected = {turn===TURNS.X}>{TURNS.X}</Square>
         <Square isSelected = {turn===TURNS.O}>{TURNS.O}</Square>
       </section>
-      {
-        winner !== null && (
-        <section className='winner'>
-          <div className='text'>
-            <h2>
-              {
-                winner === false
-                ? 'Empate': 'Gano:'
-              }
-            </h2>
-            <header className='win'>
-              {winner && <Square>{winner}</Square>}
-            </header>
-            <footer>
-              <button onClick={resetGame}>Empezar de nuevo</button>
-            </footer>
-          </div>
-          
-        </section>
-      )
-      }
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
   )
 }
---
+++ 02-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,17 @@
+import { WINNER_COMBOS } from "../constants"
+
+
+export const checkWinnerFrom = (boardToCheck) =>{
+    //revisamos todas las combinaciones ganadoras para ver si x u o ganó
+    for(const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
